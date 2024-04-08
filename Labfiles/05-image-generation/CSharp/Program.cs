3839a5e137d68c2bc86b6bd0e5a1ed96fe8c0d76
Nikhil <40881492+gsvn32@users.noreply.github.com>
2024-04-07 22:16:25 -0500

    update lab 5
    Send prompt and receive DALL E generated image url

--- Labfiles/05-image-generation/CSharp/Program.cs
+++ Labfiles/05-image-generation/CSharp/Program.cs
@@ -23,40 +23,40 @@ namespace generate_image
                 IConfigurationRoot configuration = builder.Build();
                 aoaiEndpoint = configuration["AzureOAIEndpoint"] ?? "";
                 aoaiKey = configuration["AzureOAIKey"] ?? "";
-
+                while (true) { 
                 // Get prompt for image to be generated
-                Console.Clear();
                 Console.WriteLine("Enter a prompt to request an image:");
                 string prompt = Console.ReadLine() ?? "";
 
-                // Call the DALL-E model
-                using (var client = new HttpClient())
-                {
-                    var contentType = new MediaTypeWithQualityHeaderValue("application/json");
-                    var api = "openai/deployments/dalle3/images/generations?api-version=2024-02-15-preview";
-                    client.BaseAddress = new Uri(aoaiEndpoint);
-                    client.DefaultRequestHeaders.Accept.Add(contentType);
-                    client.DefaultRequestHeaders.Add("api-key", aoaiKey);
-                    var data = new
+                    // Call the DALL-E model
+                    using (var client = new HttpClient())
                     {
-                        prompt=prompt,
-                        n=1,
-                        size="1024x1024"
-                    };
+                        var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                        var api = "openai/deployments/dalle3/images/generations?api-version=2024-02-15-preview";
+                        client.BaseAddress = new Uri(aoaiEndpoint);
+                        client.DefaultRequestHeaders.Accept.Add(contentType);
+                        client.DefaultRequestHeaders.Add("api-key", aoaiKey);
+                        var data = new
+                        {
+                            prompt = prompt,
+                            n = 1,
+                            size = "1024x1024"
+                        };
 
-                    var jsonData = JsonSerializer.Serialize(data);
-                    var contentData = new StringContent(jsonData, Encoding.UTF8, "application/json");
-                    var response = await client.PostAsync(api, contentData); 
+                        var jsonData = JsonSerializer.Serialize(data);
+                        var contentData = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                        var response = await client.PostAsync(api, contentData);
 
-                    // Get the revised prompt and image URL from the response
-                    var stringResponse = await response.Content.ReadAsStringAsync();
-                    JsonNode contentNode = JsonNode.Parse(stringResponse)!;
-                    JsonNode dataCollectionNode = contentNode!["data"];
-                    JsonNode dataNode = dataCollectionNode[0]!;
-                    JsonNode revisedPrompt = dataNode!["revised_prompt"];
-                    JsonNode url = dataNode!["url"];
-                    Console.WriteLine(revisedPrompt.ToJsonString());
-                    Console.WriteLine(url.ToJsonString().Replace(@"\u0026", "&"));
+                        // Get the revised prompt and image URL from the response
+                        var stringResponse = await response.Content.ReadAsStringAsync();
+                        JsonNode contentNode = JsonNode.Parse(stringResponse)!;
+                        JsonNode dataCollectionNode = contentNode!["data"];
+                        JsonNode dataNode = dataCollectionNode[0]!;
+                        JsonNode revisedPrompt = dataNode!["revised_prompt"];
+                        JsonNode url = dataNode!["url"];
+                        Console.WriteLine(revisedPrompt.ToJsonString());
+                        Console.WriteLine(url.ToJsonString().Replace(@"\u0026", "&"));
+                    }
 
                 }
             }
